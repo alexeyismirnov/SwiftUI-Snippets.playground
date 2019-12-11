62ff278d60cab2c6961946a7124d4ad2de6880c1
IY0018 <alexey@ironyun.com>
2019-12-11 11:23:00 +0800

    SwiftUILabView

--- Contents.swift
+++ Contents.swift
@@ -2,12 +2,4 @@ import UIKit
 import PlaygroundSupport
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("SwiftUI in a Playground!")
-    }
-}
-
-let viewController = UIHostingController(rootView: ContentView())
-
-PlaygroundPage.current.liveView = viewController
+PlaygroundPage.current.liveView = UIHostingController(rootView: SwiftUILabView())
